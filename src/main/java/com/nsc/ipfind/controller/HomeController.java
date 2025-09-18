6f9a62a052286691fcbc696d30b9f2fafdb0fde2
wlk <2838064158@qq.com>
2025-09-18 19:29:58 +0800

    项目

---
+++ src/main/java/com/nsc/ipfind/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nsc.ipfind.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/login.html";
+    }
+}
